9a046d3ee906f32b2c87522569a6a5433f18e322
satani99 <42287151+satani99@users.noreply.github.com>
2022-02-22 09:21:17 +0530

    Add files via upload

--- Brain_tumor_detection.py
+++ Brain_tumor_detection.py
@@ -17,4 +17,44 @@ def crop_brain_contour(image, plot=False):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
 
-    
+    thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.erode(thresh, None, iterations=2)
+    thresh = cv2.dilate(thresh, None, iterations=2)
+
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    c = max(cnts, key=cv2.contourArea)
+
+    extLeft = tuple(c[c[:, :, 0].argmin()][0])
+    extRight = tuple(c[c[:, :, 0].argmax()][0])
+    extTop = tuple(c[c[:, :, 1].argmin()][0])
+    extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+    new_image = image[extTop[1]:extBot[1], extLeft[0]:extRight[0]]
+
+
+    if plot:
+        plt.figure()
+
+        plt.subplot(1, 2, 1)
+        plt.imshow(image)
+
+        plt.tick_params(axis='both', which='both', top=False, bottom=False, left=False, right=False,
+        labelbottom=False, labeltop=False, labelleft=False, labelright=False)
+        plt.title('Original Image')
+
+        plt.subplot(1, 2, 2)
+        plt.imshow(new_image)
+
+        plt.tick_params(axis='both', which='both', top=False, bottom=False, left=False, right=False,
+        labelbottom=False, labeltop=False, labelleft=False, labelright=False)
+
+        plt.title('Cropped Image')
+
+        plt.show()
+
+    return new_image
+
+ex_img = cv2.imread('yes/Y1.jpg')
+ex_new_img = crop_brain_contour(ex_img, True)
+
